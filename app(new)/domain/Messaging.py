e14a5516ff4221a5318a153034c1c47f97cd6dab
daehyun99 <daehyun99@kakao.com>
2025-06-17 16:24:21 +0900

    Docs: 하위 클래스 주석 작성 #67
    - DataObject
    - Messaging
    -Register
    -Translate
    - User

--- app(new)/domain/Messaging.py
+++ app(new)/domain/Messaging.py
@@ -16,3 +16,43 @@ class Messaging:
             메시지를 전송하는 추상 메서드입니다.
     """
     ...
+
+# ======================================================= #
+
+class Email(Messaging):
+    """
+    이메일 기반 메시지 전송을 담당하는 Messaging의 하위 클래스.
+
+    SMTP 등을 통해 이메일 메시지를 전송하며, `channel` 속성은 "email"로 설정됩니다.
+    이 클래스는 메시지의 상태 확인(info) 및 실제 전송(send) 메서드를 구현합니다.
+
+    Attributes:
+        channel (str): 전송 채널. "email"로 고정.
+
+    Methods:
+        info() -> str:
+            이메일 메시지 전송 상태를 반환합니다.
+
+        send() -> None:
+            이메일 메시지를 전송합니다.
+    """
+
+
+class kakaoAPI(Messaging):
+    """
+    (추후 개발)
+    카카오톡 API를 활용한 메시지 전송을 담당하는 Messaging의 하위 클래스.
+
+    Kakao Developers API 등을 사용해 사용자에게 메시지를 전달하며,
+    `channel` 속성은 "kakao"로 설정됩니다.
+
+    Attributes:
+        channel (str): 전송 채널. "kakao"로 고정.
+
+    Methods:
+        info() -> str:
+            카카오톡 메시지 전송 상태를 반환합니다.
+
+        send() -> None:
+            카카오톡 메시지를 전송합니다.
+    """
